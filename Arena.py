77668fd8e38b9c73ffb0c01240f405f29949f322
cestpasphoto <cestpasphoto@gmail.com>
2021-02-10 18:52:14 +0100

    Better tqdm prints

--- Arena.py
+++ Arena.py
@@ -27,7 +27,7 @@ class Arena():
         self.game = game
         self.display = display
 
-    def playGame(self, verbose=False):
+    def playGame(self, verbose=False, other_way=False):
         """
         Executes one episode of a game.
 
@@ -37,19 +37,25 @@ class Arena():
             or
                 draw result returned from the game that is neither 1, -1, nor 0.
         """
-        players = [self.player2, None, self.player1]
+        if other_way:
+            players = [self.player1, None, self.player2]
+        else:
+            players = [self.player2, None, self.player1]
         curPlayer = 1
         board = self.game.getInitBoard()
         it = 0
         while self.game.getGameEnded(board, curPlayer) == 0:
             it += 1
             if verbose:
-                assert self.display
+                if self.display:
+                    self.display(board)
                 print("Turn ", str(it), "Player ", str(curPlayer))
-                self.display(board)
             action = players[curPlayer + 1](self.game.getCanonicalForm(board, curPlayer))
+                
+            canonical_board = self.game.getCanonicalForm(board, curPlayer)
+            action = players[curPlayer + 1](canonical_board)
+            valids = self.game.getValidMoves(canonical_board, 1)
 
-            valids = self.game.getValidMoves(self.game.getCanonicalForm(board, curPlayer), 1)
 
             if valids[action] == 0:
                 log.error(f'Action {action} is not valid!')
@@ -57,9 +63,10 @@ class Arena():
                 assert valids[action] > 0
             board, curPlayer = self.game.getNextState(board, curPlayer, action)
         if verbose:
-            assert self.display
+            if self.display:
+                self.display(board)
             print("Game over: Turn ", str(it), "Result ", str(self.game.getGameEnded(board, 1)))
-            self.display(board)
+            
         return curPlayer * self.game.getGameEnded(board, curPlayer)
 
     def playGames(self, num, verbose=False):
@@ -72,29 +79,24 @@ class Arena():
             twoWon: games won by player2
             draws:  games won by nobody
         """
-
-        num = int(num / 2)
         oneWon = 0
         twoWon = 0
         draws = 0
-        for _ in tqdm(range(num), desc="Arena.playGames (1)"):
-            gameResult = self.playGame(verbose=verbose)
-            if gameResult == 1:
+        t = tqdm(range(num), desc="Arena.playGames", ncols=100)
+        for i in t:
+            t.set_description('Arena ' + ('(2 vs 1)' if i>=num/2 else '(1 vs 2)'))
+            one_vs_two = (i < num/2)
+            gameResult = self.playGame(verbose=verbose, other_way=not one_vs_two)
+            if gameResult == (1 if one_vs_two else -1):
                 oneWon += 1
-            elif gameResult == -1:
+            elif gameResult == (-1 if one_vs_two else 1):
                 twoWon += 1
             else:
                 draws += 1
 
-        self.player1, self.player2 = self.player2, self.player1
-
-        for _ in tqdm(range(num), desc="Arena.playGames (2)"):
-            gameResult = self.playGame(verbose=verbose)
-            if gameResult == -1:
-                oneWon += 1
-            elif gameResult == 1:
-                twoWon += 1
-            else:
-                draws += 1
+            t.set_postfix(one_wins=oneWon, two_wins=twoWon, refresh=False)
+            ratio = oneWon / (oneWon+twoWon) if oneWon+twoWon>0 else 0.5
+            t.colour='green' if ratio>=0.55 else ('red' if ratio<=0.45 else 'yellow') #'#e59400'
+        t.close()
 
         return oneWon, twoWon, draws
