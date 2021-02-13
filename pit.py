d735b0b6326efbfb6ccd357d186fac490fa556e8
cestpasphoto <cestpasphoto@gmail.com>
2021-02-13 10:47:50 +0100

    Add ELO-like ranking computation (based on glicko-2)

--- pit.py
+++ pit.py
@@ -11,6 +11,7 @@ import numpy as np
 from utils import *
 import os.path
 from os import stat
+import glicko2
 
 """
 use this script to play any two agents against each other, or play manually with
@@ -60,6 +61,51 @@ def play(args):
 def display(numpy_board):
 	print_board(Board(2, numpy_board))
 
+def compute_elo(args):
+	def print_ratings(data):
+		ratings = [(model, data[model]['rating'], data[model]['rd']) for model in data]
+		ratings = sorted(ratings, key=lambda x: x[1])
+		for r in ratings:
+			print(f'{round(r[1]):>4} ±{round(r[2]):<3} {r[0]}')
+
+	import json
+	filename = 'glicko.json'
+	data = json.load(open(filename, 'r'))
+	if len(data.keys()) == 0:
+		rnd = glicko2.Player()
+		data['random'] = {'rating': rnd.rating, 'rd': rnd.rd, 'vol': rnd.vol}
+
+	p1_name = args.player1
+	p1_results, other_results = [], {}
+	for opponent in data.keys():
+		args.player2 = opponent
+		other_results[opponent] = []
+		results = play(args)
+		for _ in range(results[0]): # WIN
+			p1_results.append( (data[opponent]['rating'], data[opponent]['rd'], 1) )
+			other_results[opponent].append(0)
+		for _ in range(results[1]): # LOSS
+			p1_results.append( (data[opponent]['rating'], data[opponent]['rd'], 0) )
+			other_results[opponent].append(1)
+		for _ in range(results[2]): # DRAW
+			p1_results.append( (data[opponent]['rating'], data[opponent]['rd'], 0.5) )
+			other_results[opponent].append(0.5)
+
+	# Changes on p1
+	p1 = glicko2.Player()
+	p1.update_player([x[0] for x in p1_results], [x[1] for x in p1_results], [x[2] for x in p1_results])
+	data[p1_name] = {'rating': p1.rating, 'rd': p1.rd, 'vol': p1.vol}
+	# Changes on others
+	for opponent in [d for d in data.keys() if d != p1_name]:
+		p2 = glicko2.Player(data[opponent]['rating'], data[opponent]['rd'], data[opponent]['vol'])
+		p2_results = other_results[opponent]
+		p2.update_player([p1.rating]*len(p2_results), [p1.rd]*len(p2_results), p2_results)
+		data[opponent] = {'rating': p2.rating, 'rd': p2.rd, 'vol': p2.vol}
+
+	print_ratings(data)
+	with open(filename, 'w') as file:
+		json.dump(data, file, indent=2)
+
 def profiling(args):
 	import cProfile, pstats
 
@@ -86,6 +132,7 @@ def main():
 	parser.add_argument('--num-games'  , '-n' , action='store', default=30   , type=int  , help='')
 	parser.add_argument('--profile'           , action='store_true', help='enable profiling')
 	parser.add_argument('--display'           , action='store_true', help='display')
+	parser.add_argument('--elo'         , '-e', action='store_true', help='compute elo-like ranking (glicko-2)')
 
 	parser.add_argument('--numMCTSSims', '-m' , action='store', default=5   , type=int  , help='Number of games moves for MCTS to simulate.')
 	parser.add_argument('--cpuct'      , '-c' , action='store', default=1.0  , type=float, help='')
@@ -97,6 +144,8 @@ def main():
 	
 	if args.profile:
 		profiling(args)
+	elif args.elo:
+		compute_elo(args)
 	else:
 		play(args)
 
