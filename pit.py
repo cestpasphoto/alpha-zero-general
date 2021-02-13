df96223a37723f9e8a6db26c8cc16db5c8d8c011
cestpasphoto <cestpasphoto@gmail.com>
2021-02-13 10:23:51 +0100

    Remove some uneeded pit.py: tensorboard, multiple players1

--- pit.py
+++ pit.py
@@ -4,7 +4,6 @@ from splendor.SplendorPlayers import *
 from splendor.SplendorGame import SplendorGame as Game
 from splendor.SplendorLogic import Board, print_board
 from splendor.NNet import NNetWrapper as NNet
-from torch.utils.tensorboard import SummaryWriter
 
 import numpy as np
 from utils import *
@@ -45,43 +44,19 @@ def play(args):
 	if os.path.isdir(args.player2):
 		args.player2 += '/best.pt'
 	p2_name = os.path.basename(os.path.dirname(args.player2))
+	if os.path.isdir(args.player1):
+		args.player1 += '/best.pt'
+	p1_name = os.path.basename(os.path.dirname(args.player1))
 
 	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress tensorflow warning about cuda not available
 	results = []
 
-	for p1 in args.player1:
-		if p1.endswith('/'):
-			p1 = p1[:-1]
-		if os.path.isdir(p1):
-			p1_list = []
-			writer = SummaryWriter(log_dir='logs/'+os.path.basename(p1), purge_step=0)
-			for root, dirs, files in os.walk(p1):
-				for file in files:
-					if file.startswith('checkpoint') and file.endswith('.pt'): 
-						p1_list.append(p1+'/'+file)
-			p1_list = sorted(p1_list, key=lambda name: os.path.basename(name).split('.')[0][11:])
-		else:
-			writer = None
-			p1_list = [p1]
-
-		for i, single_p1 in enumerate(p1_list):
-			print(single_p1, 'vs', args.player2)
-			player1, player2 = create_player(single_p1, args), create_player(args.player2, args)
-			human = 'human' in [single_p1, args.player2]
-			arena = Arena.Arena(player1, player2, game, display=display)
-			result = arena.playGames(args.num_games, verbose=args.display or human)
-			results.append(result)
-			if writer is not None:
-				net = NNet(game)
-				cpt_dir, cpt_file = os.path.split(single_p1)
-				net.load_checkpoint(cpt_dir, cpt_file)
-				runtime = net.cumulated_uptime + net.begin_time
-				num = int(cpt_file.split('.')[0][11:])
-				score = (result[0]+result[2]/2) / args.num_games
-				if i == 0:
-					writer.add_scalar('perf_vs_'+p2_name, 0, 0, net.begin_time)
-				writer.add_scalar('perf_vs_'+p2_name, score, num, runtime)
-	return results
+	print(args.player1, 'vs', args.player2)
+	player1, player2 = create_player(args.player1, args), create_player(args.player2, args)
+	human = 'human' in [args.player1, args.player2]
+	arena = Arena.Arena(player1, player2, game, display=display)
+	result = arena.playGames(args.num_games, verbose=args.display or human)
+	return result
 
 def display(numpy_board):
 	print_board(Board(2, numpy_board))
@@ -117,7 +92,7 @@ def main():
 	parser.add_argument('--numMCTSSims', '-m' , action='store', default=5   , type=int  , help='Number of games moves for MCTS to simulate.')
 	parser.add_argument('--cpuct'      , '-c' , action='store', default=1.0  , type=float, help='')
 
-	parser.add_argument('--player1'    , '-p' , action='store', default=None , nargs='*', help='P1: either file or human, greedy, random')
+	parser.add_argument('--player1'    , '-p' , action='store', default=None , help='P1: either file or human, greedy, random')
 	parser.add_argument('--player2'    , '-P' , action='store', default=None , help='P2: either file or human, greedy, random')
 
 	args = parser.parse_args()
