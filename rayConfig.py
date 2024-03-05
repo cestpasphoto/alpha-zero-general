74962829bfc42f8b1aff1a3563dce909926bddef
cestpasphoto <cestpasphoto@gmail.com>
2024-03-05 08:46:04 +0100

    Update Ray scripts

--- rayConfig.py
+++ rayConfig.py
@@ -2,12 +2,10 @@ from ray import tune, train
 import tempfile
 from math import log10
 from subprocess import run
-from os.path import isfile
+from os.path import isfile, dirname, abspath
 
-# learn_prefix = './main.py -m 400 -e 200 -p 2 -i 2 -s 2 -n 2 -F -d 1.0 -T 10 -V 31 -P 4 --useray'
-# pit_prefix = './pit.py -m 400 -n 100 --useray'
-learn_prefix = '/home/best/dev/splendor/alpha-zero-general/main.py -m 600 -e 100 -p 2 -i 2 -s 2 -n 2 -F -d 1.0 -T 10 -V 31 -P 4 --useray'
-pit_prefix = '/home/best/dev/splendor/alpha-zero-general/pit.py -m 400 -n 50 --useray'
+learn_prefix = './main.py -m 200 -e 200 -d 3.0 -T 4 -V 83 -p 2 -i 2 -s 2 -n 2 -F -P 8 --useray'
+pit_prefix = './pit.py -m 200 -n 100 --useray'
 
 def core_func(config, prev_dir, next_dir, comp_dir, temp_dir):
 	if not isfile(f'{prev_dir}/temp.pt'):
@@ -21,12 +19,12 @@ def core_func(config, prev_dir, next_dir, comp_dir, temp_dir):
 	learn_cmdline += ['-L', prev_dir+'/'+pt, '-C', next_dir]
 	learn_cmdline += learn_args.split(' ')
 	print(' '.join(learn_cmdline))
-	run(learn_cmdline)
+	run(learn_cmdline, cwd=dirname(abspath(__file__)))
 
 	pt = 'best.pt' if isfile(f'{next_dir}/best.pt') else 'temp.pt'
 	pit_cmdline = pit_prefix.split(' ') + [comp_dir+'/best.pt', next_dir+'/'+pt]
 	print(' '.join(pit_cmdline))
-	run(pit_cmdline)
+	run(pit_cmdline, cwd=dirname(abspath(__file__)))
 	with open(f'{next_dir}/score.txt', 'r') as f:
 		score = f.read()
 		score = float(score)
@@ -49,14 +47,14 @@ def myfunc(config, args, temp_dir):
 
 def gen_params(args):
 	param_space={
-		'cpuct'     : tune.uniform(0.3  , 1.0 ),
-		#'fpu'       : 0.2,
-		'dropout'   : tune.uniform(0.1  , 0.5 ),
+		'cpuct'     : tune.uniform(0.3  , 2.0 ),
+		'fpu'       : tune.uniform(0.03 , 0.3 ),
+		'dropout'   : tune.uniform(0.   , 0.5 ),
 		# 'log_bsz'   : tune.randint(4    , 10  ),
 		# 'learn_rate': tune.uniform(3e-5 , 3e-4),
-		'q_weight'  : tune.uniform(0.4 , 0.80 ),
+		'q_weight'  : tune.uniform(0.1  , 1.0 ),
 		# 'temp'      : tune.uniform(1.1  , 1.4 ),
-		'universes' : tune.randint(1    , 6   ),
+		'universes' : tune.randint(0    , 7   ),
 	}
 
 	param_bounds = {
@@ -71,14 +69,14 @@ def gen_params(args):
 	}
 
 	param_init = {
-		'cpuct'     : tune.uniform(0.3 , 1.0 ),
-		'fpu'       : 0.2,
-		'dropout'   : 0.25,
-		'log_bsz'   : 5,   # tune.randint(5    , 8  ),
-		'learn_rate': 1e-5,
-		'q_weight'  : tune.uniform(0.4 , 0.8 ),
+		'cpuct'     : 1.0,
+		'fpu'       : 0.1,
+		'dropout'   : tune.uniform(0.   , 0.5 ),
+		'log_bsz'   : 5,
+		'learn_rate': 1e-4,
+		'q_weight'  : tune.uniform(0.1  , 1.0 ),
 		'temp'      : 1.25,
-		'universes' : tune.randint(1    , 4  ),
+		'universes' : tune.randint(0    , 7   ),
 	}
 
 	return param_init, param_space, param_bounds
