11e33e5231a25fab5fff0b9640e3f01e069930de
cestpasphoto <cestpasphoto@gmail.com>
2024-03-09 09:22:59 +0100

    Switch to botanik

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import coloredlogs
 import argparse
 
 from Coach import Coach
-from smallworld.SmallworldGame import SmallworldGame as Game
-from smallworld.NNet import NNetWrapper as nn
+from botanik.BotanikGame import BotanikGame as Game
+from botanik.NNet import NNetWrapper as NNet
 from utils import *
 import subprocess
 log = logging.getLogger(__name__)
