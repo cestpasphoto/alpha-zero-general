1f5f06fe84ac4144ad7835ac09238fc8d908b400
Jonas De Meulenaere <jonas.demeulenaere@gmail.com>
2024-03-26 09:39:41 +0100

    Dynamic imports + modify instructions

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python3
 
+import argparse
+import importlib
 import logging
 import os
+import subprocess
+
 import coloredlogs
-import argparse
 
 from Coach import Coach
-from botanik.BotanikGame import BotanikGame as Game
-from botanik.NNet import NNetWrapper as NNet
-from utils import *
-import subprocess
+
 log = logging.getLogger(__name__)
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
+
 def run(args):
+	match args.game:
+		case "botanik":
+			Game = importlib.import_module('botanik.BotanikGame').BotanikGame
+			NNet = importlib.import_module('botanik.NNet').NNetWrapper
+		case "minivilles":
+			Game = importlib.import_module('minivilles.MinivillesGame').MinivillesGame
+			NNet = importlib.import_module('minivilles.NNet').NNetWrapper
+		case "santorini":
+			Game = importlib.import_module('santorini.SantoriniGame').SantoriniGame
+			NNet = importlib.import_module('santorini.NNet').NNetWrapper
+		case "smallworld":
+			Game = importlib.import_module('smallworld.SmallworldGame').Smallworld
+			NNet = importlib.import_module('smallworld.NNet').NNetWrapper
+		case "splendor":
+			Game = importlib.import_module('splendor.SplendorGame').SplendorGame
+			NNet = importlib.import_module('splendor.NNet').NNetWrapper
+		case "thelittleprince":
+			Game = importlib.import_module('thelittleprince.TLPGame').TLPGame
+			NNet = importlib.import_module('thelittleprince.NNet').NNetWrapper
+		case _:
+			raise Exception('Please specify a game name')
+
 	log.debug('Loading %s...', Game.__name__)
 	g = Game()
 
@@ -49,10 +72,13 @@ def run(args):
 		# Backup code used for this run
 		subprocess.run(f'mkdir -p "{args.checkpoint}/"', shell=True)
 		subprocess.run(f'cp *py santorini/*py "{args.checkpoint}/"', shell=True)
-		subprocess.run(f'[ -f "{args.checkpoint}/settings.txt" ] && mv "{args.checkpoint}/settings.txt" "{args.checkpoint}/settings."`date +%s` ;   echo "{args}" > "{args.checkpoint}/settings.txt"', shell=True)
+		subprocess.run(
+			f'[ -f "{args.checkpoint}/settings.txt" ] && mv "{args.checkpoint}/settings.txt" "{args.checkpoint}/settings."`date +%s` ;   echo "{args}" > "{args.checkpoint}/settings.txt"',
+			shell=True)
 
 	log.debug('Starting the learning process 🎉')
 	c.learn()
+
 
 # Compare current settings and settings of checkpoints, display main differences
 def compare_settings(args):
@@ -61,12 +87,13 @@ def compare_settings(args):
 	# Load settings
 	if not os.path.isfile(settings_file):
 		return
-	with open(settings_file,'r') as f:
+	with open(settings_file, 'r') as f:
 		previous_args = f.read()
-    
+
 	# Compute differences on dict versions
-	previous_args_dict, current_args_dict = vars(eval('argparse.'+previous_args)), vars(args)
-	changed_keys = set([k for k in set(list(previous_args_dict.keys()) + list(current_args_dict.keys())) if previous_args_dict.get(k) != current_args_dict.get(k)])
+	previous_args_dict, current_args_dict = vars(eval('argparse.' + previous_args)), vars(args)
+	changed_keys = set([k for k in set(list(previous_args_dict.keys()) + list(current_args_dict.keys())) if
+	                    previous_args_dict.get(k) != current_args_dict.get(k)])
 	for key in ['load_folder_file', 'checkpoint', 'numIters', 'arenaCompare', 'maxlenOfQueue', 'load_model']:
 		changed_keys.discard(key)
 
@@ -75,11 +102,12 @@ def compare_settings(args):
 		for k in changed_keys:
 			print(f'{k}: {previous_args_dict.get(k)} --> {current_args_dict.get(k)}')
 
+
 def profiling(args):
-	import cProfile, pstats
+	import cProfile
 	profiler = cProfile.Profile()
 	# import yappi
-	args.parallel_inferences, args.numIters, args.numEps, args.epochs = 1, 1, 8, 1 # warmup run
+	args.parallel_inferences, args.numIters, args.numEps, args.epochs = 1, 1, 8, 1  # warmup run
 	run(args)
 
 	print('\nstart profiling')
@@ -107,48 +135,66 @@ def profiling(args):
 
 	breakpoint()
 
+
 def main():
 	parser = argparse.ArgumentParser(description='tester')
-	parser.add_argument('--checkpoint'      , '-C' , action='store', default='./temp/', help='')
-	parser.add_argument('--load-folder-file', '-L' , action='store', default=None     , help='')
-	
-	parser.add_argument('--numEps'          , '-e' , action='store', default=500  , type=int  , help='Number of complete self-play games to simulate during a new iteration')
-	parser.add_argument('--numItersHistory' , '-i' , action='store', default=5   , type=int  , help='')
+	parser.add_argument('game', action='store', default='splendor', help='The name of the game to simulate')
+	parser.add_argument('--checkpoint', '-C', action='store', default='./temp/', help='')
+	parser.add_argument('--load-folder-file', '-L', action='store', default=None, help='')
 
-	parser.add_argument('--numMCTSSims'     , '-m' , action='store', default=1600 , type=int  , help='Number of moves for MCTS to simulate in FULL exploration')
-	parser.add_argument('--tempThreshold'   , '-T' , action='store', default=10   , type=int  , help='Nb of moves after which changing temperature')
-	parser.add_argument('--temperature'     , '-t' , action='store', default=[1.25, 0.8], type=float, nargs=2, help='Softmax temp: 1 = to apply before MCTS, 3 = after MCTS, only used for selection not for learning')
-	parser.add_argument('--cpuct'           , '-c' , action='store', default=1.25 , type=float, help='cpuct value')
-	parser.add_argument('--dirichletAlpha'  , '-d' , action='store', default=-1   , type=float, help='α=0.3 for chess, scaled in inverse proportion to the approximate number of legal moves in a typical position. 0 to disable. -1 for auto.')
-	parser.add_argument('--fpu'             , '-f' , action='store', default=0.   , type=float, help='Value for FPU (first play urgency): negative value for absolute value, positive value for parent-based reduction')
-	parser.add_argument('--forced-playouts' , '-F' , action='store_true', help='Enabled forced playouts')
-	
-	parser.add_argument('--learn-rate'      , '-l' , action='store', default=0.0003, type=float, help='')
-	parser.add_argument('--epochs'          , '-p' , action='store', default=2    , type=int  , help='')
-	parser.add_argument('--batch-size'      , '-b' , action='store', default=32   , type=int  , help='')
-	parser.add_argument('--dropout'         , '-D' , action='store', default=0.   , type=float  , help='')
-	parser.add_argument('--nn-version'      , '-V' , action='store', default=1    , type=int  , help='Which architecture to choose')
+	parser.add_argument('--numEps', '-e', action='store', default=500, type=int,
+	                    help='Number of complete self-play games to simulate during a new iteration')
+	parser.add_argument('--numItersHistory', '-i', action='store', default=5, type=int, help='')
+
+	parser.add_argument('--numMCTSSims', '-m', action='store', default=1600, type=int,
+	                    help='Number of moves for MCTS to simulate in FULL exploration')
+	parser.add_argument('--tempThreshold', '-T', action='store', default=10, type=int,
+	                    help='Nb of moves after which changing temperature')
+	parser.add_argument('--temperature', '-t', action='store', default=[1.25, 0.8], type=float, nargs=2,
+	                    help='Softmax temp: 1 = to apply before MCTS, 3 = after MCTS, only used for selection not for learning')
+	parser.add_argument('--cpuct', '-c', action='store', default=1.25, type=float, help='cpuct value')
+	parser.add_argument('--dirichletAlpha', '-d', action='store', default=-1, type=float,
+	                    help='α=0.3 for chess, scaled in inverse proportion to the approximate number of legal moves in a typical position. 0 to disable. -1 for auto.')
+	parser.add_argument('--fpu', '-f', action='store', default=0., type=float,
+	                    help='Value for FPU (first play urgency): negative value for absolute value, positive value for parent-based reduction')
+	parser.add_argument('--forced-playouts', '-F', action='store_true', help='Enabled forced playouts')
+
+	parser.add_argument('--learn-rate', '-l', action='store', default=0.0003, type=float, help='')
+	parser.add_argument('--epochs', '-p', action='store', default=2, type=int, help='')
+	parser.add_argument('--batch-size', '-b', action='store', default=32, type=int, help='')
+	parser.add_argument('--dropout', '-D', action='store', default=0., type=float, help='')
+	parser.add_argument('--nn-version', '-V', action='store', default=1, type=int, help='Which architecture to choose')
 
 	### Advanced params ###
-	parser.add_argument('--q-weight'        , '-q' , action='store', default=0.5  , type=float, help='Weight for mixing Q into value loss')
-	parser.add_argument('--updateThreshold'        , action='store', default=0.60 , type=float, help='During arena playoff, new neural net will be accepted if threshold or more of games are won')
-	parser.add_argument('--ratio-fullMCTS'         , action='store', default=5    , type=int  , help='Ratio of MCTS sims between full and fast exploration')
-	parser.add_argument('--prob-fullMCTS'          , action='store', default=0.25 , type=float, help='Probability to choose full MCTS exploration')
-	parser.add_argument('--universes'       , '-u' , action='store', default=1    , type=int  , choices=range(9), help='Number of universes (up to 8); will switch between each of them at each rollout. Set to 0 for a deterministic exploration')
+	parser.add_argument('--q-weight', '-q', action='store', default=0.5, type=float,
+	                    help='Weight for mixing Q into value loss')
+	parser.add_argument('--updateThreshold', action='store', default=0.60, type=float,
+	                    help='During arena playoff, new neural net will be accepted if threshold or more of games are won')
+	parser.add_argument('--ratio-fullMCTS', action='store', default=5, type=int,
+	                    help='Ratio of MCTS sims between full and fast exploration')
+	parser.add_argument('--prob-fullMCTS', action='store', default=0.25, type=float,
+	                    help='Probability to choose full MCTS exploration')
+	parser.add_argument('--universes', '-u', action='store', default=1, type=int, choices=range(9),
+	                    help='Number of universes (up to 8); will switch between each of them at each rollout. Set to 0 for a deterministic exploration')
 
-	parser.add_argument('--forget-examples'        , action='store_true', help='Do not load previous examples')
-	parser.add_argument('--numIters'        , '-n' , action='store', default=50   , type=int, help='')
-	parser.add_argument('--stop-after-N-fail', '-s', action='store', default=-1   , type=float, help='Number of consecutive failed arenas that will trigger process stop (-N means N*numItersHistory)')
-	parser.add_argument('--profile'                , action='store_true', help='profiler')
-	parser.add_argument('--debug'                  , action='store_true', help='Disable all optimisations to allow easier debugging')
-	parser.add_argument('--useray'                 , action='store_true', help='Mode for "ray", disable some messages')
-	parser.add_argument('--parallel-inferences','-P',action='store', default=8    , type=int  , help='Size of batch for inferences = nb of threads, set to 1 to disable')
-	parser.add_argument('--no-compression'         , action='store_true', help='Prevent using in-memory data compression (huge memory decrease and impact by only by ~1 second per 100k samples), useful for easier debugging')
-	parser.add_argument('--no-mem-optim'           , action='store_true', help='Prevent cleaning MCTS tree of old moves during each game')
-	
+	parser.add_argument('--forget-examples', action='store_true', help='Do not load previous examples')
+	parser.add_argument('--numIters', '-n', action='store', default=50, type=int, help='')
+	parser.add_argument('--stop-after-N-fail', '-s', action='store', default=-1, type=float,
+	                    help='Number of consecutive failed arenas that will trigger process stop (-N means N*numItersHistory)')
+	parser.add_argument('--profile', action='store_true', help='profiler')
+	parser.add_argument('--debug', action='store_true', help='Disable all optimisations to allow easier debugging')
+	parser.add_argument('--useray', action='store_true', help='Mode for "ray", disable some messages')
+	parser.add_argument('--parallel-inferences', '-P', action='store', default=8, type=int,
+	                    help='Size of batch for inferences = nb of threads, set to 1 to disable')
+	parser.add_argument('--no-compression', action='store_true',
+	                    help='Prevent using in-memory data compression (huge memory decrease and impact by only by ~1 second per 100k samples), useful for easier debugging')
+	parser.add_argument('--no-mem-optim', action='store_true',
+	                    help='Prevent cleaning MCTS tree of old moves during each game')
+
 	args = parser.parse_args()
 	args.arenaCompare = 30
-	args.maxlenOfQueue = int(2.5e6/((2 if args.no_compression else 0.5)*args.numItersHistory)) # at most 2GB per process, with each example weighing 2kB (or 0.5kB)
+	args.maxlenOfQueue = int(2.5e6 / ((
+		                                  2 if args.no_compression else 0.5) * args.numItersHistory))  # at most 2GB per process, with each example weighing 2kB (or 0.5kB)
 	if args.stop_after_N_fail < 0:
 		args.stop_after_N_fail = -args.stop_after_N_fail * args.numItersHistory
 
@@ -167,6 +213,7 @@ def main():
 		if not args.useray:
 			print(args)
 		run(args)
+
 
 if __name__ == "__main__":
 	main()
--- santorini/SantoriniGame.py
+++ santorini/SantoriniGame.py
@@ -1,71 +1,72 @@
 import sys
+
 sys.path.append('..')
 from Game import Game
-from .SantoriniLogicNumba import Board, observation_size, action_size
-from .SantoriniDisplay import move_to_str, print_board
+from SantoriniLogicNumba import Board, observation_size, action_size
+from SantoriniDisplay import move_to_str, print_board
 import numpy as np
 
 NUMBER_PLAYERS = 2
 
+
 class SantoriniGame(Game):
-    def __init__(self):
-        self.board = Board(NUMBER_PLAYERS)
-        self.num_players = 2
+	def __init__(self):
+		self.board = Board(NUMBER_PLAYERS)
+		self.num_players = 2
 
-    def getInitBoard(self):
-        self.board.init_game()
-        return self.board.get_state()
+	def getInitBoard(self):
+		self.board.init_game()
+		return self.board.get_state()
 
-    def getBoardSize(self):
-        return observation_size()
+	def getBoardSize(self):
+		return observation_size()
 
-    def getActionSize(self):
-        return action_size()
+	def getActionSize(self):
+		return action_size()
 
-    def getNextState(self, board, player, action, random_seed=0):
-        self.board.copy_state(board, True)
-        next_player = self.board.make_move(action, player, random_seed)
-        return (self.board.get_state(), next_player)
+	def getNextState(self, board, player, action, random_seed=0):
+		self.board.copy_state(board, True)
+		next_player = self.board.make_move(action, player, random_seed)
+		return (self.board.get_state(), next_player)
 
+	def getValidMoves(self, board, player):
+		self.board.copy_state(board, False)
+		return self.board.valid_moves(player)
 
-    def getValidMoves(self, board, player):
-        self.board.copy_state(board, False)
-        return self.board.valid_moves(player)
+	def getGameEnded(self, board, next_player):
+		self.board.copy_state(board, False)
+		return self.board.check_end_game(next_player)
 
-    def getGameEnded(self, board, next_player):
-        self.board.copy_state(board, False)
-        return self.board.check_end_game(next_player)
+	def getScore(self, board, player):
+		self.board.copy_state(board, False)
+		return self.board.get_score(player)
 
-    def getScore(self, board, player):
-        self.board.copy_state(board, False)
-        return self.board.get_score(player)
+	def getRound(self, board):
+		self.board.copy_state(board, False)
+		return self.board.get_round()
 
-    def getRound(self, board):
-        self.board.copy_state(board, False)
-        return self.board.get_round()
+	def getCanonicalForm(self, board, player):
+		if player == 0:
+			return board
 
-    def getCanonicalForm(self, board, player):
-        if player == 0:
-            return board
+		self.board.copy_state(board, True)
+		self.board.swap_players(player)
+		return self.board.get_state()
 
-        self.board.copy_state(board, True)
-        self.board.swap_players(player)
-        return self.board.get_state()
+	def getSymmetries(self, board, pi, valid_actions):
+		self.board.copy_state(board, True)
+		return self.board.get_symmetries(np.array(pi, dtype=np.float32), valid_actions)
 
-    def getSymmetries(self, board, pi, valid_actions):
-        self.board.copy_state(board, True)
-        return self.board.get_symmetries(np.array(pi, dtype=np.float32), valid_actions)
+	def stringRepresentation(self, board):
+		return board.tobytes()
 
-    def stringRepresentation(self, board):
-        return board.tobytes()
+	def getNumberOfPlayers(self):
+		return NUMBER_PLAYERS
 
-    def getNumberOfPlayers(self):
-        return NUMBER_PLAYERS
+	def moveToString(self, move, current_player):
+		return move_to_str(move, current_player)
 
-    def moveToString(self, move, current_player):
-        return move_to_str(move, current_player)
-
-    def printBoard(self, numpy_board):
-        board = Board(self.getNumberOfPlayers())
-        board.copy_state(numpy_board, False)
-        print_board(board)
+	def printBoard(self, numpy_board):
+		board = Board(self.getNumberOfPlayers())
+		board.copy_state(numpy_board, False)
+		print_board(board)
