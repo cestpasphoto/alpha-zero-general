9908eb0c58cfd64aab5cd0304d980d5a3dd7f3b0
Peter Finn <peterfinn98@gmail.com>
2025-06-15 22:34:10 +0100

    Added human player

--- kingdomino/KingPlayers.py
+++ kingdomino/KingPlayers.py
@@ -19,43 +19,15 @@ class HumanPlayer():
 
     def play(self, board, nb_moves):
         valids = self.game.getValidMoves(board, 0)
-        _ = input("Continue? ")
-        action = random.choices(range(self.game.getActionSize()), weights=valids.astype(np.int8), k=1)[0]
-        return action
-
-
-class GreedyPlayer():
-    def __init__(self, game):
-        self.game = game
-
-    def play(self, board, _nb_moves):
-        valids = self.game.getValidMoves(board, 0)
-        best_move = None
-        best_to_line = 0
-        best_to_floor = 0
-        for move in range(len(valids)):
-            if valids[move]:
-                factory = board[3 + (move // 30)]
-                colour = (move % 30) // 6
-                line = move % 6
-                num_tiles = factory[colour]
-                if line == 5:
-                    to_line = 0
-                    to_floor = num_tiles
-                else:
-                    num_on_line = board[11][line]
-                    to_line = min(line + 1 - num_on_line, num_tiles)
-                    to_floor = num_tiles - to_line
-                if not best_move:
-                    best_move = move
-                    best_to_line = to_line
-                    best_to_floor = to_floor
-                elif to_line > best_to_line:
-                    best_move = move
-                    best_to_line = to_line
-                    best_to_floor = to_floor
-                elif to_line == best_to_line and to_floor < best_to_floor:
-                    best_move = move
-                    best_to_line = to_line
-                    best_to_floor = to_floor
-        return best_move
+        move = -1
+        while not move in np.where(valids)[0]:
+            moveinput = input("Full describe move? (y/n): ")
+            if moveinput == "y":
+                xcoord = int(input("x coord relative to center?: "))
+                ycoord = int(input("y coord relative to center?: "))
+                rightup = int(input("Right side up? (0/1): "))
+                pointingright = int(input("Pointing Right? (0/1): "))
+                move = 5 + (4 * (13 * (6-ycoord) + (6+xcoord)) + (2*rightup + pointingright))
+            elif moveinput == "n":
+                move = int(input("Move: "))
+        return move
